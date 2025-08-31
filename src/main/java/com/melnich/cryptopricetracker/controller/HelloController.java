c4c1574203d3746e6db9dc5844074edabc813b38
Melnich22 <mmissahogbe@gmail.com>
2025-08-31 17:35:54 +0200

    Initialisierung/Erstellung des Projektes/Erster Test

---
+++ src/main/java/com/melnich/cryptopricetracker/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.melnich.cryptopricetracker.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/ok")
+    public String hello() {
+        return "Hello World";
+    }
+}
---
+++ src/test/java/com/melnich/cryptopricetracker/CryptoPriceTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.melnich.cryptopricetracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoPriceTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
